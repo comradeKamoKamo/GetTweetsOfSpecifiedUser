36246d6b83c1bc494824cb4780900f29fb3bef37
T.T <tamutamuphone@gmail.com>
2018-07-18 23:35:18 +0900

    完成。

--- GetTweets.py
+++ GetTweets.py
@@ -1,4 +1,4 @@
-import json , os
+import json , csv
 from collections import OrderedDict
 import tweepy
 
@@ -15,14 +15,20 @@ class GetTweets:
 
         auth = tweepy.OAuthHandler(api_key,api_secret)
         auth.set_access_token(access_token,access_token_secret)
-        self.api = tweepy.API(auth)
+        self.__api = tweepy.API(auth)
 
-    def get_tweets(self,username,exclude_replies):
+    def get_tweets(self,username,exclude_replies=True):
         self.tweets = []
-        for tweet in tweepy.Cursor(self.api.user_timeline,
+        for tweet in tweepy.Cursor(self.__api.user_timeline,
             screen_name = username,
             exclude_replies = exclude_replies).items():
-            self.tweets.append(
-                [tweet.id,tweet.created_at,tweet.text.replace('\n',''),
-                tweet.favorite_count,tweet.retweet_count])
-        return self.tweets
+            if (not tweet.retweeted) and ('RT @' not in tweet.text):
+                self.tweets.append(
+                    [tweet.id,tweet.created_at,tweet.text.replace('\n',''),
+                    tweet.favorite_count,tweet.retweet_count])
+
+    def save_tweets(self,path_save):
+        with open(path_save,"w",encoding="utf-8") as f:
+            writer = csv.writer(f,lineterminator='\n')
+            writer.writerow(["id","created_at","text","fav","RT"])
+            writer.writerows(self.tweets)
