09539c008cadc7cb9cad99d118974d4444a5becf
T.T <tamutamuphone@gmail.com>
2018-07-18 23:06:58 +0900

    get_tweets実装。少しコンストラクタ修正

--- GetTweets.py
+++ GetTweets.py
@@ -5,9 +5,9 @@ import tweepy
 class GetTweets:
 
     def __init__(self,path_json):
-        s = open(path_json).read()
-        d = json.loads(s)
-
+        d = OrderedDict()
+        with open(path_json,"r") as f:
+            d = json.loads(f.read())
         api_key = d["api_key"]
         api_secret = d["api_secret"]
         access_token = d["access_token"]
@@ -17,6 +17,12 @@ class GetTweets:
         auth.set_access_token(access_token,access_token_secret)
         self.api = tweepy.API(auth)
 
-
-
-        
+    def get_tweets(self,username,exclude_replies):
+        self.tweets = []
+        for tweet in tweepy.Cursor(self.api.user_timeline,
+            screen_name = username,
+            exclude_replies = exclude_replies).items():
+            self.tweets.append(
+                [tweet.id,tweet.created_at,tweet.text.replace('\n',''),
+                tweet.favorite_count,tweet.retweet_count])
+        return self.tweets
