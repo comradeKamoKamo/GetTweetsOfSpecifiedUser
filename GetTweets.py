5c9f4897b6b73b0115feb0fe1fa62b54c7b70887
KamoKamo <tamutamuphone@gmail.com>
2018-09-13 16:51:56 +0900

    API規制回避の機能を追加

--- GetTweets.py
+++ GetTweets.py
@@ -1,4 +1,4 @@
-import json , csv
+import json , csv , time
 from collections import OrderedDict
 import tweepy
 
@@ -17,7 +17,7 @@ class GetTweets:
         auth.set_access_token(access_token,access_token_secret)
         self.__api = tweepy.API(auth)
 
-    def get_tweets(self,username,exclude_replies=True):
+    def get_tweets(self,username,exclude_replies=True,avoid_api_regulation=False):
         self.tweets = []
         for tweet in tweepy.Cursor(self.__api.user_timeline,
             screen_name = username,
@@ -26,6 +26,9 @@ class GetTweets:
                 self.tweets.append(
                     [tweet.id,tweet.created_at,tweet.text.replace('\n',''),
                     tweet.favorite_count,tweet.retweet_count])
+            # User Timeline API is allowed us to get 900 tweets / 900 sec (15 minutes)
+            if avoid_api_regulation:
+                time.sleep(1)
 
     def save_tweets(self,path_save):
         with open(path_save,"w",encoding="utf-8") as f:
