09a223282357a3f60d47136184c87942b74df837
KamoKamo <tamutamuphone@gmail.com>
2018-09-28 17:22:36 +0900

    改行処理を変更

--- GetTweets.py
+++ GetTweets.py
@@ -17,14 +17,18 @@ class GetTweets:
         auth.set_access_token(access_token,access_token_secret)
         self.__api = tweepy.API(auth)
 
-    def get_tweets(self,username,exclude_replies=True,avoid_api_regulation=False):
+    def get_tweets(self,username,exclude_replies=True,avoid_api_regulation=False,replace_newline=True):
         self.tweets = []
         for tweet in tweepy.Cursor(self.__api.user_timeline,
             screen_name = username,
             exclude_replies = exclude_replies).items():
             if (not tweet.retweeted) and ('RT @' not in tweet.text):
+                if replace_newline:
+                    text = tweet.text.replace('\n','')
+                else:
+                    text = tweet.text
                 self.tweets.append(
-                    [tweet.id,tweet.created_at,tweet.text.replace('\n',''),
+                    [tweet.id,tweet.created_at,text,
                     tweet.favorite_count,tweet.retweet_count])
             # User Timeline API is allowed us to get 900 tweets / 900 sec (15 minutes)
             if avoid_api_regulation:
